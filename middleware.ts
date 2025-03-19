d09db1772368d5a1d04e21b20f23b1f2527b396e
abhimali <abhijeetmali268@gmail.com>
2025-03-19 00:22:27 -0700

    Making changes in middleware for clerk

--- middleware.ts
+++ middleware.ts
@@ -2,17 +2,17 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware();
 
-export const config = {
-  matcher: [
-    // Match all paths except static files and _next
-    "/((?!.+\\.[\\w]+$|_next).*)",
-    // Match root path
-    "/",
-    // Match all API routes
-    "/api/:path*",
-    // Match all trpc routes
-    "/trpc/:path*",
-    // Explicitly match the generate-itinerary API route
-    "/api/generate-itinerary",
-  ],
-};
+// export const config = {
+//   matcher: [
+//     // Match all paths except static files and _next
+//     "/((?!.+\\.[\\w]+$|_next).*)",
+//     // Match root path
+//     "/",
+//     // Match all API routes
+//     "/api/:path*",
+//     // Match all trpc routes
+//     "/trpc/:path*",
+//     // Explicitly match the generate-itinerary API route
+//     "/api/generate-itinerary",
+//   ],
+//};
